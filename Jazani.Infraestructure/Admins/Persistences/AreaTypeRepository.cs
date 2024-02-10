b0eeb8349eea91ed133a76d23f67dba3f8ca60f5
Ines Katia <150395y@gmail.com>
2024-02-10 14:48:20 -0500

    feat(Domain): add CrudRepository

--- Jazani.Infraestructure/Admins/Persistences/AreaTypeRepository.cs
+++ Jazani.Infraestructure/Admins/Persistences/AreaTypeRepository.cs
@@ -1,41 +1,14 @@
 ﻿using Jazani.Domain.Admins.Models;
 using Jazani.Domain.Admins.Repositories;
 using Jazani.Infrastructure.Cores.Contexts;
-using Microsoft.EntityFrameworkCore;
+using Jazani.Infrastructure.Cores.Persistances;
 
 namespace Jazani.Infrastructure.Admins.Persistences
 {
-    public class AreaTypeRepository : IAreaTypeRepository
+    public class AreaTypeRepository : CrudRepository<AreaType, int>, IAreaTypeRepository
     {
-        private readonly ApplicationDbContext _dbContext;
-
-        public AreaTypeRepository(ApplicationDbContext dbContext)
+        public AreaTypeRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
-            _dbContext = dbContext;
-        }
-
-        public async Task<IReadOnlyList<AreaType>> FindAllAsync()
-        {
-            return await _dbContext.AreaTypes.ToListAsync();
-        }
-
-        public async Task<AreaType?> FindByIdAsync(int id)
-        {
-            return await _dbContext.AreaTypes.FirstOrDefaultAsync(x => x.Id == id);
-        }
-
-        public async Task<AreaType> SaveAsync(AreaType areaType)
-        {
-            EntityState entityState = _dbContext.Entry(areaType).State;
-            
-            _ = entityState switch
-            {
-                EntityState.Detached => _dbContext.AreaTypes.Add(areaType),
-                EntityState.Modified => _dbContext.AreaTypes.Update(areaType),
-                
-            };
-            await _dbContext.SaveChangesAsync();
-            return areaType;
         }
     }
 }
