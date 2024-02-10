da3a0e387b9866d19da2d2badbbf047c19887608
Ines Katia <150395y@gmail.com>
2024-02-09 21:47:57 -0500

    feat(Domain):add base structure and AreaType

---
+++ Jazani.Domain/Admins/Models/AreaType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jazani.Domain.Admins.Models
+{
+    public class AreaType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;//obligatorio que tenga un valor
+        public string?  Description{ get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool State { get;set; }
+
+    }
+}
---
+++ Jazani.Domain/Admins/Repositories/IAreaTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using Jazani.Domain.Admins.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jazani.Domain.Admins.Repositories
+{
+    public interface IAreaTypeRepository
+    {
+        Task<IReadOnlyList<AreaType>> FindAllAsync();
+        Task<AreaType?> FindByIdAsync(int id);
+        Task<AreaType>SaveAsync(AreaType areaType);
+
+    }
+}
